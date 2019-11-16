bbf1f6e7011be6d697f39eeaf9c05c5e2922a29e
doaa-altarawy <doaa.altarawy@gmail.com>
2019-11-16 14:38:02 -0500

    Fix login formating bootstrap 3 and 4

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ def read_requirements():
 
 if __name__ == "__main__":
     setuptools.setup(
-        name='QCArchive_website',
+        name='QCArchive_webapps',
         version="0.1.0",
         description='Website for the QCArchive: the Quantum Chemistry Archive',
         author='Doaa Altarawy',
         author_email='daltarawy@vt.edu',
-        url="https://github.com/MolSSI/QCArchive_website",
+        url="https://github.com/MolSSI/QCArchive_webapps",
         license='BSD-3C',
 
         packages=setuptools.find_packages(),
