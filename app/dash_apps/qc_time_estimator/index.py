bf05c60e4cc2c23d8f84f37e70de4de21fcd9672
Matthew Welborn <mattgwelborn@gmail.com>
2020-02-27 15:52:15 -0500

    Fills in some sensible defaults

--- app/dash_apps/qc_time_estimator/index.py
+++ app/dash_apps/qc_time_estimator/index.py
@@ -29,7 +29,7 @@ def get_basis_sets_options():
 
 
 def required_tag():
-    return html.Span('*', className='text-danger')
+    return html.Span("*", className="text-danger")
 
 
 class QCTimeEstimatorApp(DashAppBase):
@@ -44,153 +44,175 @@ class QCTimeEstimatorApp(DashAppBase):
 
         super().__init__(*args, **kwargs)
 
-
     layout = html.Div(
         [
             # dbc.Alert(
             #     "The app is in a pre-alpha state and is for demonstration purposes only.",
             #     color="warning",
             # ),
-
             ### Header
-            dbc.Row([dbc.Col([html.H3("QC Execution Time Estimator")])],
-                    className='mb-5'),
-
-            dbc.Alert("Please enter required fields with valid values.",
-                id='error-msg', color="danger", is_open=False, dismissable=True,),
-
+            dbc.Row(
+                [dbc.Col([html.H3("QC Execution Time Estimator")])], className="mb-5"
+            ),
+            dbc.Alert(
+                "Please enter required fields with valid values.",
+                id="error-msg",
+                color="danger",
+                is_open=False,
+                dismissable=True,
+            ),
             ### Main selectors
-
             dbc.Row(
                 [
-                    dbc.Col([dbc.Label("Molecule (Formula, SMILES or InChI)"), required_tag()], width=3),
                     dbc.Col(
-                        dcc.Input(id='molecule', type='text', size=50),
+                        [
+                            dbc.Label("Molecule (Formula, SMILES or InChI)"),
+                            required_tag(),
+                        ],
+                        width=3,
+                    ),
+                    dbc.Col(
+                        dcc.Input(id="molecule", type="text", size=50, value="H2O"),
                     ),
                 ],
-                className='mb-2',
+                className="mb-2",
             ),
             dbc.Row(
                 [
                     dbc.Col([dbc.Label("Choose method"), required_tag()], width=3),
-                    dbc.Col([
-                            dcc.Input(id="method", size=50)
-                    ]),
+                    dbc.Col([dcc.Input(id="method", size=50, value="B3LYP")]),
                 ],
-                className='mb-2'
+                className="mb-2",
             ),
             dbc.Row(
                 [
                     dbc.Col([dbc.Label("Choose Basis Set"), required_tag()], width=3),
-                    dbc.Col([
+                    dbc.Col(
+                        [
                             dcc.Dropdown(
                                 id="basis-set",
-                                value='',
+                                value="6-31G",
                                 options=get_basis_sets_options(),
                             )
-                    ]),
+                        ]
+                    ),
                 ],
-                className='mb-2',
+                className="mb-2",
             ),
-
             dbc.Row(
                 [
                     dbc.Col([dbc.Label("Select Driver:"), required_tag()], width=3),
-                    dbc.Col([
-                        dcc.Dropdown(id="driver",
-                                     options=[{"label": k, "value": k} for k in DriverEnum],
-                        ),
-                    ]),
+                    dbc.Col(
+                        [
+                            dcc.Dropdown(
+                                id="driver",
+                                options=[{"label": k, "value": k} for k in DriverEnum],
+                                value="energy",
+                            ),
+                        ]
+                    ),
                 ],
-                className='mb-2',
+                className="mb-2",
             ),
-
-            dbc.Row([
+            dbc.Row(
+                [
                     dbc.Col([], width=3),
                     dbc.Col(
                         dcc.Checklist(
-                        id='restricted',
-                        options=[
-                            {'label': 'Restricted', 'value': 'restricted'},
-                        ],
-                        value=['restricted'],
-                        inputClassName='mr-2'
+                            id="restricted",
+                            options=[{"label": "Restricted", "value": "restricted"},],
+                            value=["restricted"],
+                            inputClassName="mr-2",
                         ),
                     ),
                 ],
-                className='mb-2',
+                className="mb-2",
             ),
-
             dbc.Row(
                 [
                     dbc.Col([dbc.Label("Number of threads:")], width=3),
-                    dbc.Col([
-                        dcc.Input(id="threads", value=1, size=25),
-                    ]),
+                    dbc.Col([dcc.Input(id="threads", value=1, size=25),]),
                 ],
-                className='mb-2',
+                className="mb-2",
             ),
-
             dbc.Row(
                 [
-                    dbc.Col([dbc.Label("CPU Clock Speed (GHz):"), required_tag()], width=3),
-                    dbc.Col([
-                        dcc.Input(id="cpu-clock-speed",
-                                  type='number',
-                                  min=0.5, max=7, step=0.1,
-                                  size=25,  # TODO: Does not work!!
-                                  # required=True
-                        )
-                                     # options=[{"label": f'{k/1000} GHz', "value": k} for k in range(500, 7500, 500)]),
-                    ], width=2),
-                    html.Span('GHz'),
+                    dbc.Col(
+                        [dbc.Label("CPU Clock Speed (GHz):"), required_tag()], width=3
+                    ),
+                    dbc.Col(
+                        [
+                            dcc.Input(
+                                id="cpu-clock-speed",
+                                type="number",
+                                min=0.5,
+                                max=7,
+                                step=0.1,
+                                value=2.2,
+                                size=25,  # TODO: Does not work!!
+                                # required=True
+                            )
+                            # options=[{"label": f'{k/1000} GHz', "value": k} for k in range(500, 7500, 500)]),
+                        ],
+                        width=2,
+                    ),
+                    html.Span("GHz"),
                 ],
-                className='mb-2',
+                className="mb-2",
             ),
-
             dbc.Row(
                 [
                     dbc.Col([dbc.Label("CPU Launch Year:"), required_tag()], width=3),
-                    dbc.Col([
-                        dcc.Dropdown(id="cpu-launch-year", options=[{"label": k, "value": k} for k in range(2020, 2000, -1)]),
-                    ]),
+                    dbc.Col(
+                        [
+                            dcc.Dropdown(
+                                id="cpu-launch-year",
+                                options=[
+                                    {"label": k, "value": k}
+                                    for k in range(2020, 2000, -1)
+                                ],
+                                value=2019,
+                            ),
+                        ]
+                    ),
                 ],
-                className='mb-5',
+                className="mb-5",
             ),
-
             html.Hr(),
-
             dbc.Row(
                 [
-                    dbc.Col([
-                        html.Button('Submit', id='submit', className='float-right btn-primary'),
+                    dbc.Col(
+                        [
+                            html.Button(
+                                "Submit",
+                                id="submit",
+                                className="float-right btn-primary",
+                            ),
                         ],
                     ),
                 ],
-                className='mb-5',
+                className="mb-5",
             ),
-
             html.Hr(),
-
             dbc.Card(
                 [
                     dcc.Loading(
                         id="loading-1",
                         children=[
-                            html.P(["Estimated Execution Time: ",
-                                    html.Span(id='wall-time')
-                            ]),
-                            html.P(["Model Version: ",
-                                    html.Span(id='model-version'),
-                            ]),
+                            html.P(
+                                [
+                                    "Estimated Execution Time: ",
+                                    html.Span(id="wall-time"),
+                                ]
+                            ),
+                            html.P(["Model Version: ", html.Span(id="model-version"),]),
                             # dcc.Graph(id="primary-graph")
                         ],
                         type="default",
-                        className='p-3'
+                        className="p-3",
                     )
                 ]
             ),
-
         ],
         className="container",
     )
@@ -201,53 +223,64 @@ class QCTimeEstimatorApp(DashAppBase):
     def register_callbacks(self, dashapp):
         @dashapp.callback(
             [
-                Output('wall-time', 'children'),
-                Output('model-version', 'children'),
-                Output('error-msg', 'is_open'),
+                Output("wall-time", "children"),
+                Output("model-version", "children"),
+                Output("error-msg", "is_open"),
             ],
-            [Input('submit', 'n_clicks')],
+            [Input("submit", "n_clicks")],
             [
-                State('molecule', 'value'),
-                State('method', 'value'),
-                State('basis-set', 'value'),
-                State('driver', 'value'),
-                State('restricted', 'value'),
-                State('threads', 'value'),
-                State('cpu-clock-speed', 'value'),
-                State('cpu-launch-year', 'value'),
-            ]
+                State("molecule", "value"),
+                State("method", "value"),
+                State("basis-set", "value"),
+                State("driver", "value"),
+                State("restricted", "value"),
+                State("threads", "value"),
+                State("cpu-clock-speed", "value"),
+                State("cpu-launch-year", "value"),
+            ],
         )
-        def update_output(n_clicks, molecule, method, basis_set, driver,
-                          restricted, threads, cpu_clock_speed, cpu_launch_year):
+        def update_output(
+            n_clicks,
+            molecule,
+            method,
+            basis_set,
+            driver,
+            restricted,
+            threads,
+            cpu_clock_speed,
+            cpu_launch_year,
+        ):
 
             if not n_clicks:
-                return ['', '' , False]
+                return ["", "", False]
 
             data = dict(
                 molecule=molecule,
                 method=method,
                 basis_set=basis_set,
                 driver=driver,
-                restricted=True if 'restricted' in restricted else False,
+                restricted=True if "restricted" in restricted else False,
                 threads=threads if threads else 1,
-                cpu_clock_speed=cpu_clock_speed*1000 if cpu_clock_speed else None,
+                cpu_clock_speed=cpu_clock_speed * 1000 if cpu_clock_speed else None,
                 cpu_launch_year=cpu_launch_year,
             )
 
-            logger.info(f'Running prediction for input: {data}')
+            logger.info(f"Running prediction for input: {data}")
 
             for k, v in data.items():
                 if v is None:
-                    return ['', '', True]
+                    return ["", "", True]
 
             if not current_model_exists():
-                return ['(running model training, please try again later)', '', False]
+                return ["(running model training, please try again later)", "", False]
 
             try:
                 ret = make_prediction(input_data=[data])
-                return [natural.date.compress(ret['predictions'][0]*3600), ret['version'], False]
+                return [
+                    natural.date.compress(ret["predictions"][0] * 3600),
+                    ret["version"],
+                    False,
+                ]
             except Exception as err:
                 logger.error(f"Error in make_prediction.\n{err}")
-                return ['', '', True]
-
-
+                return ["", "", True]
