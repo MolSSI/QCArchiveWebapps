6f1a4fbb3c67e5cd0c38c71c3024628f4a31333b
Matthew Welborn <mattgwelborn@gmail.com>
2020-03-09 18:54:16 -0400

    try to fix nginx kill issue

--- nginx/boot.sh
+++ nginx/boot.sh
@@ -2,4 +2,6 @@
 
 
 certbot --nginx --non-interactive --agree-tos -m qcarchive@molssi.org -d qcarchive-webapps.eastus.cloudapp.azure.com
+kill $(ps aux | grep '[n]ginx' | awk '{print $2}')  # certbot can't kill nginx cleanly?
+sleep 3
 nginx -g "daemon off;"
